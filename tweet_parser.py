8c6c2f71f32369746ebf88b2d90a11df22ecaa40
Cat <cat@dhcp-18-111-12-162.dyn.mit.edu>
2015-07-31 18:03:46 -0400

    update filenames

--- tweet_parser.py
+++ tweet_parser.py
@@ -37,28 +37,28 @@ def printStream():
     #stream.filter(track=['python', 'javascript', 'ruby'])
 
 
-def loadTweets(tweets_data_path='stream_test.txt'):
-    tweets_data = []
-    tweets_file = open(tweets_data_path, "r")
+def loadTweets(tweets_json_path='data/stream_test.txt'):
+    tweets_json = []
+    tweets_file = open(tweets_json_path, "r")
     for line in tweets_file:
         try:
             tweet = json.loads(line)
-            tweets_data.append(tweet)
+            tweets_json.append(tweet)
         except:
             continue
-    return tweets_data
+    return tweets_json
 
-def tweetsToPD(tweets_data):
-    tweets = pandas.DataFrame()
-    tweets['text'] = map(lambda tweet: tweet['text']\
+def tweetsToPD(tweets_json):
+    tweets_df = pandas.DataFrame()
+    tweets_df['text'] = map(lambda tweet: tweet['text']\
                          if 'text' in tweet.keys()\
-                        else None, tweets_data)
-    tweets['lang'] = map(lambda tweet: tweet['lang']\
-                        if 'lang' in tweet.keys() else None, tweets_data)
-    tweets['country'] = map(lambda tweet: tweet['place']['country'] \
+                        else None, tweets_json)
+    tweets_df['lang'] = map(lambda tweet: tweet['lang']\
+                        if 'lang' in tweet.keys() else None, tweets_json)
+    tweets_df['country'] = map(lambda tweet: tweet['place']['country'] \
                         if 'place' in tweet.keys() and tweet['place'] != None\
-                         else None, tweets_data)
-    return tweets
+                         else None, tweets_json)
+    return tweets_df
 
 def plotByLang(tweets_df):
     tweets_by_lang = tweets_df['lang'].value_counts()
@@ -71,6 +71,21 @@ def plotByLang(tweets_df):
     ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
     tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
     plt.savefig('figs/tweets_by_lang.png')
+
+
+def plotByCountry(tweets_df):
+    tweets_by_country = tweets_df['country'].value_counts()
+
+    fig, ax = plt.subplots()
+    ax.tick_params(axis='x', labelsize=15)
+    ax.tick_params(axis='y', labelsize=10)
+    ax.set_xlabel('Countries', fontsize=15)
+    ax.set_ylabel('Number of tweets' , fontsize=15)
+    ax.set_title('Top 5 countries', fontsize=15, fontweight='bold')
+    tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
+    plt.savefig('figs/tweets_by_country.png')
+
+
 
 
 
