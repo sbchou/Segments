abc34e563f474dbf671b64098ba94d72a2d0fb14
Cat <cat@dhcp-18-111-105-93.dyn.mit.edu>
2015-09-01 11:45:05 -0400

    add mongo code

--- org_baseline.py
+++ org_baseline.py
@@ -1,5 +1,4 @@
-#Import the necessary methods from tweepy library
-import twitter
+import tweepy
 import json
 import pandas 
 import matplotlib.pyplot as plt
@@ -7,65 +6,143 @@ from geopy.geocoders import GoogleV3, Nominatim
 import csv
 from progressbar import ProgressBar
 import random
-from twitter import TwitterError
+from tweepy import TweepError
 import time
 import numpy as np
 import sklearn
+import frogress
+from pymongo import MongoClient
 
 def init_twitt():
     CONSUMER_KEY = 'RE9RJs5c3zQ8yhLCZQCKlVglT'
     CONSUMER_SECRET = 'iA5ElxRl9JWm4wYDntSI2UxT56Yp6SpcDkAJ40EoDvMMFnWkfK'
-    ACCESS_KEY = '359150678-DAyroyYOpYkqLiCaNIok2M9KoY2fj1C4fxBO5v6R'
-    ACCESS_SECRET = 'zhTmAekrR0hcX4yfV4mRLVx1OQZMuIikj3dTvtfzghjAZ'
-    api = twitter.Api(consumer_key=CONSUMER_KEY,
-            consumer_secret=CONSUMER_SECRET, access_token_key=ACCESS_KEY, access_token_secret=ACCESS_SECRET)
+    #ACCESS_KEY = '359150678-DAyroyYOpYkqLiCaNIok2M9KoY2fj1C4fxBO5v6R'
+    #ACCESS_SECRET = 'zhTmAekrR0hcX4yfV4mRLVx1OQZMuIikj3dTvtfzghjAZ'
+    auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+def init_json_api():
+    CONSUMER_KEY = 'RE9RJs5c3zQ8yhLCZQCKlVglT'
+    CONSUMER_SECRET = 'iA5ElxRl9JWm4wYDntSI2UxT56Yp6SpcDkAJ40EoDvMMFnWkfK'
+    #ACCESS_KEY = '359150678-DAyroyYOpYkqLiCaNIok2M9KoY2fj1C4fxBO5v6R'
+    #ACCESS_SECRET = 'zhTmAekrR0hcX4yfV4mRLVx1OQZMuIikj3dTvtfzghjAZ'
+    auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    api = tweepy.API(auth, parser = tweepy.parsers.JSONParser())
     return api
 
 def getTraining(path='data/humanizr_data/humanizr_labeled.tsv'):
     training = pandas.DataFrame.from_csv(path, sep="\t")
     return training
 
+def saveUsers(training, api):
+    client = MongoClient('localhost', 27017)
+    db = client['twitter_db']
+    collection = db['tweeps']
+ 
+    n = len(training.index)
+    ids = list(training.index) 
+    random.shuffle(ids)
+    again = True
+    for t_id in ids: 
+        if collection.find({'id': t_id}).count() > 0: 
+            print "seen", t_id
+        else:
+            while again == True:
+                try:  
+                    user = api.get_user(t_id)
+                    collection.insert(user)
+                    print "done", t_id
+                    time.sleep(5)
+                    break
+
+                except TweepError, e: 
+                    #import pdb; pdb.set_trace()
+                    try:
+                        if e[0][0]['code'] == 88:
+                            print t_id, e, "zzZZZZZz"
+                            time.sleep(900)
+                            continue 
+                    except:
+                        print str(e)
+                        break
+
+def mongo_to_CSV():
+    client = MongoClient('localhost', 27017)
+    db = client['twitter_db']
+    collection = db['tweeps']
+    with open('data/user_feats.csv', 'wt') as f:
+        writer = csv.writer(f)
+        writer.writerow(("id", "screen_name", "name", "verified",\
+                    "description", "favourites_count", "followers_ct", \
+                    "friends_ct", "tweets_ct"))
+        for doc in collection.find({}, \
+                    {'id':1,
+                    'screen_name': 1,
+                    'name': 1,
+                    'verified':1,
+                    'description': 1,
+                    'favourites_count':1,
+                    'followers_count':1,
+                    'friends_count':1,
+                    'tweets_count':1}):
+            print doc['id']
+            writer.writerow([unicode(s).encode('utf-8')\
+                    for s in doc.values()])
+
 def getUserFeatures(training, api):
-    with open('data/userfeatures.csv', 'wt') as f:
-        pbar = ProgressBar()
+    with open('data/userfeatures2.csv', 'wt') as f:
+         
         writer = csv.writer(f)
         writer.writerow(("id", "screenname", "name", "verified",\
                 "desc", "favorites_ct", "followers_ct", \
                 "friends_ct", "tweets_ct"))
         n = len(training.index)
-        ids = list(training.index)
+        ids = list(training.index) 
         random.shuffle(ids)
         again = True
-        for t_id in pbar(ids):
+        for t_id in ids: 
+            
             while again == True:
-                try:
-                    user = api.GetUser(t_id)
-                    screenname = user.GetScreenName()
-                    name = user.GetName()
-                    verified = user.GetVerified()
-                    description = user.GetDescription()
-                    favorites_ct = user.GetFavouritesCount()
-                    followers_ct = user.GetFollowersCount()
-                    friends_ct = user.GetFriendsCount()
-                    tweets_ct = user.GetStatusesCount()
+                try: 
+                    print t_id
+                    user = api.get_user(t_id)
+                    screenname = user.screen_name
+                    name = user.name
+                    verified = user.verified
+                    description = user.description
+                    favorites_ct = user.favourites_count
+                    followers_ct = user.followers_count
+                    friends_ct = user.friends_count
+                    tweets_ct = user.statuses_count
                     writer.writerow([unicode(s).encode("utf-8") for s in (t_id, screenname, name, verified, \
                             description, favorites_ct, followers_ct, \
                             friends_ct, tweets_ct)])
                     print "done", t_id
-                    again = False
-                except TwitterError:
-                    print t_id, "zzZZZZZz"
                     time.sleep(5)
-                    again = True
-                except Exception, e:
-                    import pdb; pdb.set_trace()
-                    print str(e)
+                    break
 
+                except TweepError, e: 
+                    if e[0][0]['code'] == 88:
+                        print t_id, e, "zzZZZZZz"
+                        time.sleep(900)
+                        continue 
+                    else:
+                        print str(e)
+                        break
+                
 def runFeatureExtract():
     """Outputs the feature + label CSV"""
     api = init_twitt()
     training = getTraining()
     getUserFeatures(training, api)
+
+
+def runUserSave():
+    """Outputs the feature + label CSV"""
+    api = init_json_api()
+    training = getTraining()
+    saveUsers(training, api)
 
 def joinLabels(LABEL_PATH='data/humanizr_data/humanizr_labeled.tsv',\
                 FEATURE_PATH='data/userfeatures.csv'):
@@ -102,4 +179,6 @@ def evaluateModel(model, test_path='data/lr_test.csv'):
     print 'Accuracy: ', round(accuracy * 100, 2), '%'
 
 if __name__ == '__main__':
-    runFeatureExtract()
+    #runFeatureExtract()
+    #runUserSave()
+    mongo_to_CSV() 
